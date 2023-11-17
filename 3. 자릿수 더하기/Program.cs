eee9116c8cd7d142682cf6326a4227ed10ac058d
jyWooooo <wjy970423@gmail.com>
2023-11-17 09:37:44 +0900

    Solve 3. 자릿수 더하기

---
+++ 3. 자릿수 더하기/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int divider = 1;
+        int answer = 0;
+        while (divider <= n)
+        {
+            answer += (n / divider) % 10;
+            divider *= 10;
+        }
+
+        return answer;
+    }
+}
