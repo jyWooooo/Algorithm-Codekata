b4eaee57762c79ee1aa052d39952c88c66c2f334
jyWooooo <wjy970423@gmail.com>
2023-11-17 09:32:07 +0900

    Solve 2. 평균 구하기

---
+++ Algorithm Codekata/2. 평균 구하기.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public double solution(int[] arr)
+    {
+        double avg = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++) 
+            sum += arr[i];
+        avg = (double)sum / arr.Length;
+        return avg;
+    }
+}
