fdd40a9fec6020586d9660e3695d81eb0f6f9e17
jyWooooo <wjy970423@gmail.com>
2023-11-21 09:52:11 +0900

    Solve 19. 정수 제곱근 판별

--- 19. 정수 제곱근 판별/Program.cs
+++ 19. 정수 제곱근 판별/Program.cs
@@ -5,6 +5,6 @@ public class Solution
     public long solution(long n)
     {
         var d = Math.Sqrt(n);
-        return (long)d - d == 0 ? (long)(d * d) : -1;
+        return (long)d - d == 0 ? (long)((d + 1) * (d + 1)) : -1;
     }
 }
