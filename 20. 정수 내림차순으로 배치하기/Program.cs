278083042dc15740ad86edc0e96d593d957557e1
jyWooooo <wjy970423@gmail.com>
2023-11-21 09:25:02 +0900

    Solve 20. 정수 내림차순으로 배치하기

---
+++ 20. 정수 내림차순으로 배치하기/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public long solution(long n)
+    {
+        string str = n.ToString();
+        str = new string(str.OrderByDescending(x => x).ToArray());
+        return long.Parse(str);
+    }
+}
