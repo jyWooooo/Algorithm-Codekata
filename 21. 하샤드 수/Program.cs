764aa0d8b9aa266244ea209aae6b4d4490d9e3ca
jyWooooo <wjy970423@gmail.com>
2023-11-21 09:33:13 +0900

    Solve 21. 하샤드 수

---
+++ 21. 하샤드 수/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public bool solution(int x)
+    {
+        int sum = 0;
+        for (int i = 1; i <= x; i *= 10)
+            sum += (x % (i * 10)) / i;
+        return x % sum == 0;
+    }
+}
