5cccf66353ab4888b9b6c4ea3c031881d64d78e3
jyWooooo <wjy970423@gmail.com>
2023-11-21 09:52:24 +0900

    Solve 22. 두 정수 사이의 합

---
+++ 22. 두 정수 사이의 합/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public long cal(int a, int b)
+    {
+        long sum = 0;
+        for (int i = a; i <= b; i++)
+            sum += i;
+        return sum;
+    }
+
+    public long solution(int a, int b)
+    {
+        return a < b ? cal(a, b) : cal(b, a);
+        // 등차수열의 합 공식으로 풀면 빠름
+        //return (long)(System.Math.Abs(a - b) + 1) * (a + b) / 2;
+    }
+}
