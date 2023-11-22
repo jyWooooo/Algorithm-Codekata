20e83e382ad03d8392417a7acd30985059b755b7
jyWooooo <wjy970423@gmail.com>
2023-11-22 09:55:38 +0900

    Solve 23. 콜라츠 추측

--- 23. 콜라츠 추측/Program.cs
+++ 23. 콜라츠 추측/Program.cs
@@ -9,12 +9,10 @@ public class Solution
         while (n != 1)
         {
             n = n % 2 == 0 ? n / 2 : n * 3 + 1;
-
             cnt++;
             if (cnt >= 500)
                 return -1;
         }
-
         return cnt;
     }
 }
