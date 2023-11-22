dc4f3c38dea98629db6455f36d55d6e9cb8c0f7a
jyWooooo <wjy970423@gmail.com>
2023-11-22 09:22:19 +0900

    Solve 23. 콜라츠 추측

---
+++ 23. 콜라츠 추측/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+
+public class Solution
+{
+    public int solution(int num)
+    {
+        int cnt = 0;
+        long n = num;
+        while (n != 1)
+        {
+            n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+
+            cnt++;
+            if (cnt >= 500)
+                return -1;
+        }
+
+        return cnt;
+    }
+}
