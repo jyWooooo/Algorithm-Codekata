2c34439df00c8088c20fb7c2d8bf0cf3685c1a7c
jyWooooo <wjy970423@gmail.com>
2023-11-22 09:49:48 +0900

    Solve 25. 나누어 떨어지는 숫자 배열

---
+++ 25. 나누어 떨어지는 숫자 배열/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] arr, int divisor)
+    {
+        int[] answer = arr.Where(x => x % divisor == 0).ToArray().OrderBy(x => x).ToArray();
+        if (answer.Length == 0) answer = new int[1] { -1 };
+        return answer;
+    }
+}
