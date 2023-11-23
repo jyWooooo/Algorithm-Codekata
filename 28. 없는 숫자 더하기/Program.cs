f81b40f31a87dcf31124ecfabb4cded562bc50b5
jyWooooo <wjy970423@gmail.com>
2023-11-23 09:19:01 +0900

    Solve 28. 없는 숫자 더하기

---
+++ 28. 없는 숫자 더하기/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] numbers)
+    {
+        return 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 - numbers.Sum();
+    }
+}
