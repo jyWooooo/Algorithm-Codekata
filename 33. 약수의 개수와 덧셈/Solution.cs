032fac8c8bf3df77197985ce8506977540d1fa3a
jyWooooo <wjy970423@gmail.com>
2023-11-24 10:26:39 +0900

    Solve 33. 약수의 개수와 덧셈

---
+++ 33. 약수의 개수와 덧셈/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public int Cal(int x)
+    {
+        int cnt = 0;
+        for (int i = 1; i <= x; i++)
+        {
+            if (x % i == 0)
+                cnt++;
+        }
+        return cnt;
+    }
+
+    public int solution(int left, int right)
+    {
+        int answer = 0;
+        for (int i = left; i <= right; i++)
+        {
+            int cnt = Cal(i);
+            answer += cnt % 2 == 0 ? i : -i;
+        }
+        return answer;
+    }
+}
