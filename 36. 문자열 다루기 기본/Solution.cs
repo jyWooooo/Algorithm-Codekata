b7ecb8cac355010986e6104f6fbfebcd6e51eed6
jyWooooo <wjy970423@gmail.com>
2023-11-24 10:47:33 +0900

    Solve 36. 문자열 다루기 기본

---
+++ 36. 문자열 다루기 기본/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public bool solution(string s)
+    {
+        if (s.Length == 4 || s.Length == 6)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsDigit(s[i]))
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
