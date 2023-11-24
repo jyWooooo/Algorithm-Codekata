ea243f23243aa66818f1b7d236cc70401e1e65c1
jyWooooo <wjy970423@gmail.com>
2023-11-24 10:42:04 +0900

    Solve 35. 부족한 금액 계산하기

---
+++ 35. 부족한 금액 계산하기/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Solution
+{
+    public long solution(int price, int money, int count)
+    {
+        long total = 0;
+        for (int i = 1; i <= count; i++)
+            total += price * i;
+        return total > money ? total - money : 0;
+    }
+}
