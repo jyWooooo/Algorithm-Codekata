730a29b7665a526fb3439cbd086c562db6ae2655
jyWooooo <wjy970423@gmail.com>
2023-11-27 09:18:51 +0900

    Solve 41. 최대공약수와 최소공배수

---
+++ 41. 최대공약수와 최소공배수/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int GCD(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        else
+            return GCD(b, a % b);
+    }
+
+    public int LCM(int a, int b)
+    {
+        return a * b / GCD(a, b);
+    }
+
+    public int[] solution(int n, int m)
+    {
+        return new int[] { GCD(n, m), LCM(n, m) };
+    }
+}
