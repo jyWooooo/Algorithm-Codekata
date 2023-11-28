b754cdbc4cdc453b3470590df883db4f47cc9796
jyWooooo <wjy970423@gmail.com>
2023-11-29 08:52:51 +0900

    Solve 44. 최소 직사각형

--- 44. 최소 직사각형/Program.cs
+++ 44. 최소 직사각형/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[,] sizes)
+    {
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        for (int i = 0; i < sizes.GetLength(0); i++)
+        {
+            if (sizes[i, 0] < sizes[i, 1])
+            {
+                int temp = sizes[i, 0];
+                sizes[i, 0] = sizes[i, 1];
+                sizes[i, 1] = temp;
+            }
+
+            maxWidth = Math.Max(maxWidth, sizes[i, 0]);
+            maxHeight = Math.Max(maxHeight, sizes[i, 1]);
+        }
+
+        return maxWidth * maxHeight;
+    }
+}
