8b9e515b86285b2c1a968cc1664f8449e34d252e
jyWooooo <wjy970423@gmail.com>
2023-11-28 09:35:50 +0900

    Solve 43. 크기가 작은 부분 문자열

---
+++ 43. 크기가 작은 부분 문자열/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(string t, string p)
+    {
+        int answer = 0;
+        long n = long.Parse(p);
+        for (int i = 0; i < t.Length - p.Length + 1; i++)
+        {
+            var current = t.Substring(i, p.Length);
+            if (long.Parse(current) <= n)
+                answer++;
+        }
+        return answer;
+    }
+}
