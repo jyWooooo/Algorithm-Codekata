890b2a77629e665ac556d38c8a365d3016a7f614
jyWooooo <wjy970423@gmail.com>
2023-11-28 09:07:21 +0900

    Solve 41. 이상한 문자 만들기

---
+++ 41. 이상한 문자열 만들기/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public string solution(string s)
+    {
+        string answer = "";
+
+        var splits = s.Split(' ');
+
+        foreach(var str in splits)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                    answer += char.ToUpper(str[i]);
+                else
+                    answer += char.ToLower(str[i]);
+            }
+            answer += ' ';
+        }
+        answer = answer.Remove(answer.Length - 1);
+        return answer;
+    }
+}
