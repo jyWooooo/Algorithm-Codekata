a2b30171ce0c7b413f71e011d7e5a9a8007c5f06
jyWooooo <wjy970423@gmail.com>
2023-11-30 08:55:16 +0900

    Solve 49. 두 개 뽑아서 더하기

---
+++ 49. 두 개 뽑아서 더하기/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] numbers)
+    {
+        List<int> answer = new List<int>();
+        for (int i = 0; i < numbers.Length; i++)
+            for (int j = i + 1; j < numbers.Length; j++)
+                answer.Add(numbers[i] + numbers[j]);
+        return answer.Distinct().OrderBy(x => x).ToArray();
+    }
+}
