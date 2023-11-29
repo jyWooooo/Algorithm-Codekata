64912914de78c4624a4887a2d8395c4186593c3c
jyWooooo <wjy970423@gmail.com>
2023-11-29 09:01:22 +0900

    Solve 45. 시저 암호

---
+++ 45. 시저 암호/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public string solution(string s, int n)
+    {
+        string answer = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsLower(s[i]))
+            {
+                char c = (char)(s[i] + n > 'z' ? s[i] + n - 'z' + 'a' : s[i] + n);
+                answer += c;
+            }
+            else if (char.IsUpper(s[i]))
+            {
+                char c = (char)(s[i] + n > 'Z' ? s[i] + n - 'Z' + 'A' : s[i] + n);
+                answer += c;
+            }
+            else answer += s[i];
+        }
+
+        return answer;
+    }
+}
