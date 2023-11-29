b33de9a89b0dfa23a091d14c97361b8e1538f452
jyWooooo <wjy970423@gmail.com>
2023-11-29 09:20:41 +0900

    Solve 47. 문자열 내 마음대로 정렬하기

---
+++ 47. 문자열 내 마음대로 정렬하기/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public string[] solution(string[] strings, int n)
+    {
+        Array.Sort(strings, (a, b) => a[n].CompareTo(b[n]) == 0 ? a.CompareTo(b) : a[n].CompareTo(b[n]));
+        return strings;
+        //return strings.OrderBy(str => str).OrderBy(str => str[n]).ToArray();
+    }
+}
