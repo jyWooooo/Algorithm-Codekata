d8125a6d3ecb34ddbf79054ce342c67400b95982
jyWooooo <wjy970423@gmail.com>
2023-11-30 10:03:56 +0900

    Solve 50. 가장 가까운 같은 글자

---
+++ 50. 가장 가까운 같은 글자/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(string s)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        int[] answer = new int[s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.ContainsKey(s[i]))
+            {
+                answer[i] = i - dict[s[i]];
+                dict[s[i]] = i;
+            }
+            else
+            {
+                dict.Add(s[i], i);
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
