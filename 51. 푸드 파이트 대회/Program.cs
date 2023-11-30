fb03214d1880a1dcaa19e4c85be5c98fb0f419df
jyWooooo <wjy970423@gmail.com>
2023-11-30 10:14:50 +0900

    Solve 51. 푸드 파이트 대회

---
+++ 51. 푸드 파이트 대회/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public string solution(int[] food)
+    {
+        string answer = "";
+        for (int i = 1; i < food.Length; i++)
+            answer += new string((char)(i + '0'), food[i] / 2);
+        answer += "0" + new string(answer.Reverse().ToArray());
+        return answer;
+    }
+}
