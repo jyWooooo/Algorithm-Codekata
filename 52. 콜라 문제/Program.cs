94e48269c6b48dbca74191450f89bcba24fcf11f
jyWooooo <wjy970423@gmail.com>
2023-11-30 10:33:10 +0900

    Solve 52. 콜라 문제

---
+++ 52. 콜라 문제/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int solution(int a, int b, int n)
+    {
+        int answer = 0;
+        while (n >= a)
+        {
+            int mod = n % a;
+            int get = (n / a) * b;
+            answer += get;
+            n = mod + get;
+        }
+        return answer;
+    }
+}
