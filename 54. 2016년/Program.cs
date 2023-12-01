5964e108ff06b19745e7cf63211323e126a2de1b
jyWooooo <wjy970423@gmail.com>
2023-12-01 09:46:31 +0900

    Solve 54. 2016년

---
+++ 54. 2016년/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string solution(int a, int b)
+    {
+        int deltaDay = 0;
+        int[] month = new int[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        string[] week = new string[] { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU", };
+        for (int i = 0; i < a - 1; i++)
+            deltaDay += month[i];
+        deltaDay += b - 1;
+        deltaDay %= 7;
+        return week[deltaDay];
+
+        //System.DateTime dateTime = new System.DateTime(2016, a, b);
+        //return dateTime.ToString("ddd").ToUpper();
+    }
+}
