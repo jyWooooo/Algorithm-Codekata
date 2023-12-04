fc71eab143fff30337aca698f8f07456a6dfcf74
jyWooooo <wjy970423@gmail.com>
2023-12-04 09:58:34 +0900

    Solve 56. 과일장수

---
+++ 56. 과일장수/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int k, int m, int[] score)
+    {
+        int answer = 0;
+        Array.Sort(score, (a, b) => -a.CompareTo(b));
+        for (int i = 0; i < score.Length; i++)
+        {
+            if ((i + 1) % m == 0)
+                answer += score[i] * m;
+        }
+        return answer;
+    }
+}
