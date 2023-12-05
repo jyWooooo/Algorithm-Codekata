4c9a716a4937ac082142a113cdf2d6c86526b24e
jyWooooo <wjy970423@gmail.com>
2023-12-05 09:38:56 +0900

    Solve 58. 소수 만들기

---
+++ 58. 소수 만들기/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Solution
+{
+    public bool isPrime(int n)
+    {
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    public int solution(int[] nums)
+    {
+        int answer = 0;
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = i + 1; j < nums.Length; j++)
+                for (int k = j + 1; k < nums.Length; k++)
+                    if (isPrime(nums[i] + nums[j] + nums[k]))
+                        answer++;
+        return answer;
+    }
+}
