6206f0dd89f623388ddff19fdc15b2e806b94b63
jyWooooo <wjy970423@gmail.com>
2023-12-05 09:27:09 +0900

    Solve 57. 모의고사

---
+++ 57. 모의고사/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] answers)
+    {
+        int[][] cycles = new int[3][] {
+            new int[]{ 1, 2, 3, 4, 5 },
+            new int[]{ 2, 1, 2, 3, 2, 4, 2, 5 },
+            new int[]{ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 }
+        };
+        List<int> answer = new List<int>();
+        int[] scores = new int[3] { 0, 0, 0 };
+        for (int i = 0; i < answers.Length; i++)
+        {
+            scores[0] += answers[i] == cycles[0][i % cycles[0].Length] ? 1 : 0;
+            scores[1] += answers[i] == cycles[1][i % cycles[1].Length] ? 1 : 0;
+            scores[2] += answers[i] == cycles[2][i % cycles[2].Length] ? 1 : 0;
+        }
+        int max = scores.Max();
+        for (int i = 0; i < scores.Length; i++)
+            if (scores[i] == max)
+                answer.Add(i + 1);
+        return answer.ToArray();
+    }
+}
