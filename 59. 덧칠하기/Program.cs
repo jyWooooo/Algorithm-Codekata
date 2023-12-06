35bf8a3c7a92dc0d26e40ed0c03bffb31ef5d359
jyWooooo <wjy970423@gmail.com>
2023-12-06 09:20:37 +0900

    Solve 59. 덧칠하기

---
+++ 59. 덧칠하기/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n, int m, int[] section)
+    {
+        int answer = 0;
+        bool[] wall = new bool[n];
+        foreach (int i in section)
+            wall[i - 1] = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (wall[i])
+            {
+                for (int j = i; j < i + m && j < n; j++)
+                    wall[j] = false;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
