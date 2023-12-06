f1afbe1c1a073e8b68a7c1e9009c46902f720fba
jyWooooo <wjy970423@gmail.com>
2023-12-06 09:59:26 +0900

    Solve 60. 기사단원의 무기

---
+++ 60. 기사단원의 무기/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+//public class Solution
+//{
+//    public int DivisorCount(int n)
+//    {
+//        int cnt = 0;
+//        for (int i = 1; i <= Math.Sqrt(n); i++)
+//        {
+//            if (i * i == n)
+//                cnt++;
+//            else if (n % i == 0)
+//                cnt += 2;
+//        }
+//        return cnt;
+//    }
+
+//    public int solution(int number, int limit, int power)
+//    {
+//        int answer = 0;
+//        for (int i = 1; i <= number; i++)
+//            answer += DivisorCount(i) <= limit ? DivisorCount(i) : power;
+//        return answer;
+//    }
+//}
+
+public class Solution
+{
+    public int solution(int number, int limit, int power)
+    {
+        int answer = 0;
+        int[] divisorCounts = new int[number];
+        for (int i = 1; i <= number; i++)
+            for (int j = i; j <= number; j += i)
+                divisorCounts[j - 1]++;
+        for (int i = 0; i < divisorCounts.Length; i++)
+            answer += divisorCounts[i] <= limit ? divisorCounts[i] : power;
+            return answer;
+    }
+}
