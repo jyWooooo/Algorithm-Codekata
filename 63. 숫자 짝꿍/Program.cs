a4527a1d802e42cabeb8801f95e1bc29f0de5eb5
jyWooooo <wjy970423@gmail.com>
2023-12-08 10:49:56 +0900

    Solve 63. 숫자 짝꿍

---
+++ 63. 숫자 짝꿍/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public string solution(string X, string Y)
+    {
+        string answer = "";
+        int[] XCounts = new int[10];
+        int[] YCounts = new int[10];
+        X.Select(x => XCounts[x - '0']++).ToArray();
+        Y.Select(x => YCounts[x - '0']++).ToArray();
+        for (int i = 9; i >= 0; i--)
+            answer += new string((char)(i + '0'), Math.Min(XCounts[i], YCounts[i]));
+        if (answer.Length == 0)
+            return "-1";
+        if (answer.Max(x => x - '0') == 0)
+            return "0";
+        return answer;
+    }
+}
