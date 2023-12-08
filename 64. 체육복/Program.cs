d6afa5f3b42a6e11b339fee7188cb83a22f3ea82
jyWooooo <wjy970423@gmail.com>
2023-12-08 11:00:25 +0900

    Solve 64. 체육복

---
+++ 64. 체육복/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public int solution(int n, int[] lost, int[] reserve)
+    {
+        int answer = 0;
+        int[] student = new int[n + 2];
+        student = student.Select(x => x = 1).ToArray();
+        foreach (var e in lost) student[e]--;
+        foreach (var e in reserve) student[e]++;
+        student[0] = -1;
+        student[n + 1] = -1;
+
+        for (int i = 1; i < student.Length - 1; i++)
+        {
+            if (student[i] == 0)
+            {
+                if (student[i - 1] == 2)
+                {
+                    student[i]++;
+                    student[i - 1]--;
+                }
+                else if (student[i + 1] == 2)
+                {
+                    student[i]++;
+                    student[i + 1]--;
+                }
+            }
+            if (student[i] >= 1) answer++;
+        }
+
+        return answer;
+    }
+}
