43204e04fdc8207d54123f9442bc33e39b66ab7c
jyWooooo <wjy970423@gmail.com>
2023-12-11 09:25:31 +0900

    Solve 65. 문자열 나누기

---
+++ 65. 문자열 나누기/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = 0;
+        int[] dict = new int[2] { 0, 0 };
+        char x = '\0';
+        foreach (char c in s)
+        {
+            if (x == '\0')
+                x = c;
+            if (x == c)
+                dict[0]++;
+            else
+                dict[1]++;
+
+            if (dict[0] == dict[1])
+            {
+                answer++;
+                dict = new int[2] { 0, 0 };
+                x = '\0';
+            }
+        }    
+
+        if (dict[0] != 0)
+            answer++;
+
+        return answer;
+    }
+}
