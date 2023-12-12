2e29e2f9faf4c250aaef5bffb93fa206e81a6a3d
jyWooooo <wjy970423@gmail.com>
2023-12-12 09:50:01 +0900

    Solve 67. 둘 만의 암호

---
+++ 67. 둘 만의 암호/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public string solution(string s, string skip, int index)
+    {
+        string answer = "";
+        foreach (char c in s)
+        {
+            char newChar = c;
+            for (int i = 0; i < index; i++)
+            {
+                newChar++;
+                if (newChar > 'z') newChar = 'a';
+                if (skip.Contains(newChar))
+                    i--;
+            }
+            answer += newChar;
+        }
+        return answer;
+    }
+}
