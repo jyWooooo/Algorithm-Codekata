ec511b7c90683961be4857b648aad123a854cf2e
jyWooooo <wjy970423@gmail.com>
2023-12-13 09:52:57 +0900

    Solve 68. 햄버거 만들기

---
+++ 68. 햄버거 만들기/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int[] ingredient)
+    {
+        int answer = 0;
+        Stack<int> stack = new Stack<int>();
+
+        foreach (int item in ingredient)
+        {
+            if (item == 1 && stack.TryPeek(out var top))
+            {
+                if (top == 3)
+                {
+                    for (int i = 0; i < 3; i++)
+                        stack.Pop();
+                    answer++;
+                    continue;
+                }
+            }
+
+            if (item == 1)
+                stack.Push(item);
+            else if (stack.TryPeek(out top))
+            {
+                if (item - 1 == top)
+                    stack.Push(item);
+                else
+                    stack.Clear();
+            }
+        }
+
+        return answer;
+    }
+}
