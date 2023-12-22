e890456d475b3c75b6a565d0f4e363869c8c697a
jyWooooo <wjy970423@gmail.com>
2023-12-22 10:14:55 +0900

    Solve 75. 최대값과 최소값

---
+++ 75. 최대값과 최소값/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution
+{
+    public string solution(string s)
+    {
+        var splits = s.Split(' ');
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        foreach (var split in splits)
+        {
+            min = Math.Min(min, int.Parse(split));
+            max = Math.Max(max, int.Parse(split));
+        }
+        return $"{min} {max}";
+    }
+}
