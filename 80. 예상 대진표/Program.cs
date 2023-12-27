ca2d0e1bf609509f2328b28733208a2df70934df
jyWooooo <wjy970423@gmail.com>
2023-12-27 09:53:59 +0900

    Solve 80, 81, 82

---
+++ 80. 예상 대진표/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        int maxRound = 0;
+        while (n >= 2)
+        {
+            maxRound++;
+            n /= 2;
+        }
+
+        for (int i = 0; i < maxRound; i++)
+        {
+            answer++;
+            a = a % 2 == 0 ? a / 2 : a / 2 + 1;
+            b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+            if (a == b) return answer;
+        }
+
+        return answer;
+    }
+}
---
+++ 81. N개의 최소 공배수/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] arr)
+    {
+        int maxValue = arr.Max();
+        int currentValue = maxValue;
+        while (true)
+        {
+            bool check = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (currentValue % arr[i] != 0)
+                {
+                    check = false;
+                    break;
+                }
+            }
+            
+            if (check)
+                return currentValue;
+            else
+                currentValue += maxValue;
+        }
+    }
+}
