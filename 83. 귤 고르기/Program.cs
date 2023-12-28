f115b825c85e3599661adb8762f3cb73131c0da5
jyWooooo <wjy970423@gmail.com>
2023-12-28 10:01:25 +0900

    Solve 83, 84

---
+++ 83. 귤 고르기/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int k, int[] tangerine)
+    {
+        int answer = 0;
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < tangerine.Length; i++)
+        {
+            if (!dict.TryAdd(tangerine[i], 1))
+                dict[tangerine[i]] += 1;
+        }
+
+        var list = dict.Select(item => item.Value).OrderByDescending(item => item).ToList();
+
+        foreach (var e in list)
+        {
+            if (k <= 0)
+                break;
+
+            k -= e;
+            answer++;
+        }
+
+        return answer;
+    }
+}
---
+++ 84. 괄호 회전하기/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = 0;
+        Stack<char> stack = new Stack<char>();
+        Queue<char> queue = new Queue<char>(s);
+        for (int i = 0; i < queue.Count; i++)
+        {
+            stack.Clear();
+            foreach (var e in queue)
+            {
+                if (stack.TryPeek(out char peek))
+                {
+                    if (peek == '(' && e == ')')
+                        stack.Pop();
+                    else if (peek == '{' && e == '}')
+                        stack.Pop();
+                    else if (peek == '[' && e == ']')
+                        stack.Pop();
+                    else
+                        stack.Push(e);
+                }
+                else
+                    stack.Push(e);
+            }
+            if (stack.Count == 0)
+                answer++;
+            queue.Enqueue(queue.Dequeue());
+        }
+        return answer;
+    }
+}
