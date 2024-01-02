7e9ebe6c766146d70a412c2e490bb14636d67ca8
jyWooooo <wjy970423@gmail.com>
2024-01-02 09:30:33 +0900

    Solve 85

---
+++ 85. 연속 부분 수열 합의 개수/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int[] elements)
+    {
+        int[] sums = (int[])elements.Clone();
+        HashSet<int> set = new HashSet<int>(elements);
+
+        for (int i = 1; i < elements.Length; i++)
+        {
+            for (int j = 0; j < elements.Length; j++)
+            {
+                sums[j] += elements[(i + j) % elements.Length];
+                set.Add(sums[j]);
+            }
+        }
+
+        return set.Count;
+    }
+}
