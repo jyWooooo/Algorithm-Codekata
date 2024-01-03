8eeb5209dff228845b6815b05118fbfef545d912
jyWooooo <wjy970423@gmail.com>
2024-01-03 09:40:04 +0900

    Solve 86, 87

---
+++ 86. H-Index/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] citations)
+    {
+        Array.Sort(citations, (a, b) => b.CompareTo(a));
+
+        for (int i = 0; i < citations.Length; i++)
+        {
+            if (citations[i] < i + 1)
+                return i;
+        }
+        return citations.Length;
+    }
+}
---
+++ 87. n^2 배열 자르기/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int[] solution(int n, long left, long right)
+    {
+        int[] answer = new int[right - left + 1];
+
+        for (long i = left; i <= right; i++)
+        {
+            int temp = (i / n) >= (i % n) ? (int)(i / n + 1) : (int)(i % n + 1);
+            answer[i - left] = temp;
+        }
+
+        return answer;
+    }
+}
