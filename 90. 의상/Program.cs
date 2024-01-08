08013d8669f1224d6af125ec1b45c52d99ec46a2
jyWooooo <wjy970423@gmail.com>
2024-01-08 09:47:51 +0900

    Solve 90, 91

---
+++ 90. 의상/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(string[,] clothes)
+    {
+        int answer = 1;
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < clothes.GetLength(0); i++)
+            if (!dict.TryAdd(clothes[i, 1], 1))
+                dict[clothes[i, 1]]++;
+
+        foreach (var e in dict)
+            answer *= e.Value + 1;
+
+        return answer - 1;
+    }
+}
---
+++ 91. 기능개발/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] progresses, int[] speeds)
+    {
+        List<int> answer = new List<int>();
+        List<int> progress = progresses.ToList();
+        List<int> speed = speeds.ToList();
+
+        while (progress.Count > 0)
+        {
+            for (int i = 0; i < progress.Count; i++)
+                progress[i] += speed[i];
+
+            int a = 0;
+            for (int i = 0; i < progress.Count; i++)
+            {
+                if (progress[i] >= 100)
+                    a++;
+                else break;
+            }
+
+            progress.RemoveRange(0, a);
+            speed.RemoveRange(0, a);
+            if (a > 0) answer.Add(a);
+        }
+
+        return answer.ToArray();
+    }
+}
