188b90afe70f6c9f364a8385179688ad3a371179
jyWooooo <wjy970423@gmail.com>
2024-01-15 09:43:35 +0900

    Solve 92

---
+++ 92. 프로세스/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] priorities, int location)
+    {
+        int answer = 0;
+
+        List<int> pq = new List<int>();
+        Queue<(int idx, int priority)> q = new Queue<(int idx, int priority)>();
+
+        for (int i = 0; i < priorities.Length; i++)
+        {
+            pq.Add(priorities[i]);
+            q.Enqueue((i, priorities[i]));
+        }
+
+        pq = pq.OrderByDescending(x => x).ToList();
+
+        while (q.Count > 0)
+        {
+            (int idx, int priority) peek = q.Dequeue();
+
+            if (pq[0] == peek.priority)
+            {
+                pq.RemoveAt(0);
+                answer++;
+
+                if (peek.idx == location)
+                    break;
+            }
+            else
+                q.Enqueue(peek);
+        }
+
+        return answer;
+    }
+}
