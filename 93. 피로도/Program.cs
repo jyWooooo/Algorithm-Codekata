83c8a36e36e062b00725fd8d0710b7ac1c4cb426
jyWooooo <wjy970423@gmail.com>
2024-01-16 09:28:20 +0900

    Solve 93, 94

---
+++ 93. 피로도/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int answer = 0;
+    public bool[] visited;
+    public int n;
+
+    public void DFS(int k, bool[] visited, int[,] dungeons, int cnt)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i] && k >= dungeons[i, 0])
+            {
+                visited[i] = true;
+                DFS(k - dungeons[i, 1], visited, dungeons, cnt + 1);
+                visited[i] = false;
+            }
+        }
+        answer = Math.Max(answer, cnt);
+    }
+
+    public int solution(int k, int[,] dungeons)
+    {
+        n = dungeons.GetLength(0);
+        visited = new bool[n];
+        DFS(k, visited, dungeons, 0);
+        return answer;
+    }
+}
---
+++ 94. 타겟 넘버/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int answer = 0;
+    public void DFS(int[] nums, int target, int current, int depth)
+    {
+        if (depth == nums.Length)
+        {
+            if (current == target)
+                answer++;
+            return;
+        }
+
+        DFS(nums, target, current + nums[depth], depth++);
+        DFS(nums, target, current - nums[depth], depth++);
+    }
+
+    public int solution(int[] numbers, int target)
+    {
+        DFS(numbers, target, 0, 0);
+        return answer;
+    }
+}
