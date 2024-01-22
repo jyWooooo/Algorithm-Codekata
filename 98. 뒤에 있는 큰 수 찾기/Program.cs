b32ad193eaf1a0036abc7593dfaae4d633a4d999
jyWooooo <wjy970423@gmail.com>
2024-01-22 10:14:33 +0900

    Solve 98

---
+++ 98. 뒤에 있는 큰 수 찾기/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] numbers)
+    {
+        int[] answer = new int[numbers.Length];
+        answer = answer.Select(x => x = -1).ToArray();
+        Stack<int> stack = new Stack<int>();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            while (stack.Count != 0 && numbers[stack.Peek()] < numbers[i])
+                answer[stack.Pop()] = numbers[i];
+            stack.Push(i);
+        }
+        return answer;
+    }
+}
