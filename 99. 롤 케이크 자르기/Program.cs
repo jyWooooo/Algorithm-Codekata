1dc89d052e454947bef4affe6d6104f8dcdbb8a6
jyWooooo <wjy970423@gmail.com>
2024-01-23 10:05:27 +0900

    Solve 99

---
+++ 99. 롤 케이크 자르기/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int[] topping)
+    {
+        int answer = 0;
+        Dictionary<int, int> m1 = new Dictionary<int, int>();
+        Dictionary<int, int> m2 = new Dictionary<int, int>();
+
+        foreach (int i in topping)
+            if (!m1.TryAdd(i, 1))
+                m1[i]++;
+
+        foreach (int i in topping)
+        {
+            m1[i]--;
+            if (m1[i] == 0)
+                m1.Remove(i);
+            if (!m2.TryAdd(i, 1))
+                m2[i]++;
+
+            if (m1.Count == m2.Count)
+                answer++;
+        }
+
+        return answer;
+    }
+}
