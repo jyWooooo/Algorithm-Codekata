4ba23ae5337e91592aecca6c2a51d3b1b951ef06
jyWooooo <wjy970423@gmail.com>
2024-01-24 09:56:12 +0900

    Solve 100

---
+++ 100. 숫자 변환하기/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int x, int y, int n)
+    {
+        int[] dp = new int[y + 1];
+        dp = dp.Select(item => int.MaxValue).ToArray();
+        dp[y] = 0;
+        for (int i = y; i > 0; i--)
+        {
+            if (dp[i] == int.MaxValue)
+                continue;
+
+            if (i - n > 0)
+                dp[i - n] = Math.Min(dp[i - n], dp[i] + 1);
+            if (i % 2 == 0)
+                dp[i / 2] = Math.Min(dp[i / 2], dp[i] + 1);
+            if (i % 3 == 0)
+                dp[i / 3] = Math.Min(dp[i / 3], dp[i] + 1);
+        }
+        return dp[x] == int.MaxValue ? -1 : dp[x];
+    }
+}
