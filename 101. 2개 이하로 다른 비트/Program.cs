2b4cde95bb991054d9b2a0df19a4c9098306e3cd
jyWooooo <wjy970423@gmail.com>
2024-01-25 10:04:16 +0900

    Solve 101

---
+++ 101. 2개 이하로 다른 비트/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution
+{
+    public long[] solution(long[] numbers)
+    {
+        long[] answer = new long[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            long res = numbers[i] ^ (numbers[i] + 1);
+            res >>= 2;
+            res += numbers[i] + 1;
+            answer[i] = res;
+        }
+        return answer;
+    }
+}
