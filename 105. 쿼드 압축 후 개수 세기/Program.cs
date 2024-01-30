1d87cbe9dce4f72b630f5906558f71d84e0edd2a
jyWooooo <wjy970423@gmail.com>
2024-01-30 22:04:58 +0900

    Solve 105

---
+++ 105. 쿼드 압축 후 개수 세기/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public void DFS(int x, int y, int size, int[,] arr, int[] answer)
+    {
+        bool flag = true;
+        for (int i = x; i < x + size; i++)
+        {
+            for (int j = y; j < y + size; j++)
+            {
+                if (arr[i, j] != arr[x, y])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+
+        if (size == 1 || flag)
+        {
+            answer[arr[x, y]]++;
+            return;
+        }
+
+        size /= 2;
+        DFS(x, y, size, arr, answer);
+        DFS(x + size, y, size, arr, answer);
+        DFS(x, y + size, size, arr, answer);
+        DFS(x + size, y + size, size, arr, answer);
+    }
+
+    public int[] solution(int[,] arr)
+    {
+        int[] answer = new int[] { 0, 0 };
+        DFS(0, 0, arr.GetLength(0), arr, answer);
+        return answer;
+    }
+}
